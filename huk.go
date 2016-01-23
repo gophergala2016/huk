57291a2d4c0caea71aa17171a11618a4b8a12f3b
Neil Chudleigh <nchudleigh@gmail.com>
2016-01-23 12:45:18 -0500

    command line working, moved 'search' into client

--- huk.go
+++ huk.go
@@ -1,29 +1,38 @@
 package main
 
 import(
-    "github.com/nchudleigh/huk/search"
+    // "github.com/nchudleigh/huk/search"
     "fmt"
-    "net"
+    "strings"
+    "os"
+    "github.com/Pallinder/go-randomdata"
+    "log"
     // "server"
     // "client"
+
 )
 
 func main(){
-    // Decide between these two based on args file, key
-    // if file:
-        // server
-    // else:
-        // client
+    args := os.Args[1:]
 
-    // client
-        // Find IP by going through list (192.168.0.[1..255]:port_x)
-        // connection established
-        // generate pgp (private and public keys)
-        // send public key to server
-        // save encrypted file from stream
-        // decrypt using private key
+    var filename string
+    var key string
+    var user_type string
 
-    // server
+    if args[0] == "-f" && len(args) == 2{
+        // server case
+        filename=args[1]
+        key = strings.ToLower(randomdata.SillyName())
+    } else if len(args) == 1{
+        // client case
+        key = args[0]
+    } else {
+        // error
+        log.Fatal("I need either a filename or a key ex: '$ huk -f filename.txt' or '$ huk key'")
+    }
+
+    switch user_type{
+    case "server":
         // create server on port_x
         // listen for connections
         // validate incoming request with given key
@@ -31,10 +40,27 @@ func main(){
         // recieves clients public key
         // encrypt file using client's pub key
         // send encrypted file over stream to client
+    case "client":
+        // Find server IP by going through list (192.168.0.[1..255]:port_x)
+        // connection established
+        // generate pgp (private and public keys)
+        // send public key to server
+        // save encrypted file from stream
+        // decrypt using private key
+
+    }
 
 
-    myIp := search.GetMyLocalIP()
-    fmt.Printf("%v\n", myIp)
+    // Decide between these two based on args file, key
+    // if file:
+        // server
+    // else:
+        // client
 
-    search.FindBuddysIP()
+
+
+
+    // myIp := search.GetMyLocalIP()
+    // fmt.Printf("%v\n", myIp)
+    // search.FindBuddysIP()
 }
