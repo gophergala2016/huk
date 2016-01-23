fc9cd96badf8290213e65fbcf4e8121d0f0b1eee
Neil Chudleigh <nchudleigh@gmail.com>
2016-01-23 13:36:37 -0500

    user friendliness

--- huk.go
+++ huk.go
@@ -37,10 +37,26 @@ func main(){
         log.Fatal("I need either a filename or a key ex: '$ huk -f filename.txt' or '$ huk key'")
     }
 
-    fmt.Println("filename",filename)
-    fmt.Println("key", key)
-
     if isClient{
+        fmt.Printf(
+            "Searching for '%v' on your local network..\n",
+            key
+        )
+        // Find server IP by going through list (192.168.0.[1..255]:port_x)
+        // connection established
+        // generate pgp (private and public keys)
+        // send public key to server
+        // save encrypted file from stream
+        // decrypt using private key
+    } else {
+        fmt.Printf(
+            "The key for your file (%v) is %v.\n"+
+            "Tell your friend to run '$ huk %v'\n"+
+            "Waiting for connection...\n",
+            filename,
+            key,
+            key,
+        )
         // create server on port_x
         // listen for connections
         // validate incoming request with given key
@@ -48,20 +64,13 @@ func main(){
         // recieves clients public key
         // encrypt file using client's pub key
         // send encrypted file over stream to client
-    } else {
-        // Find server IP by going through list (192.168.0.[1..255]:port_x)
-        // connection established
-        // generate pgp (private and public keys)
-        // send public key to server
-        // save encrypted file from stream
-        // decrypt using private key
     }
 }
 
 
 func isAlpha(input string) bool {
     for _,c := range input{
-        if 'a'>c || c > 'z'{
+        if 'a' > c || c > 'z'{
             return false
         }
     }
